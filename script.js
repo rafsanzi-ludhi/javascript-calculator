835eaf296cfcd5877a087c4c28a85acf7e12a7ca
rafalos99 <rafsanzaniludhi@gmail.com>
2023-04-24 09:13:03 +0100

    updated script.js, calculator can do BIDMAS

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// script.js
 const currentInput = document.querySelector('.current-input');
-const resultOutput = document.querySelector('.result-input');
+const resultOutput = document.querySelector('.result-output');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.clear');
@@ -8,53 +7,168 @@ const deleteButton = document.querySelector('.delete');
 const answerButton = document.querySelector('.answer');
 const equalButton = document.querySelector('.equal');
 
-class Calculator { //creates a calculator class
-    constructor(currentInput, resultOutput) { 
-        this.currentInput = currentInput; //assigns the current input variable to the current input property
-        this.resultOutput = resultOutput; //assigns the result output variable to the result output property
-        this.clear(); //calls the clear method
+class Calculator {
+    constructor(currentInput, resultOutput) {
+        this.currentInputSaved = currentInput;
+        this.resultOutputSaved = resultOutput;
+        this.clear();
     }
 
-    clear() { //clears the current input and result output
-        this.currentInput = ''; //sets the current input to an empty string
-        this.resultOutput = ''; //sets the result output to an empty string
-        this.operator = undefined; //sets the operator to undefined
+    clear() {
+        this.currentInputSaved = '';
+        this.resultOutputSaved = '';
+        this.operatorSaved = undefined;
     }
 
-    delete() { //deletes the last character of the current input
-    
+    delete() {
+        this.currentInputSaved = this.currentInputSaved.toString().slice(0, -1); //converts the current input to a string and deletes the last character
     }
 
-    equal() { //calculates the result
-    
+    equal() {
+        const postfix = this.toPostfix(this.currentInputSaved);
+        const result = this.evaluatePostfix(postfix);
+        this.resultOutputSaved = result;
     }
 
-    operators() { //adds the operator to the current input
-
+    operators(operator) {
+    if (this.currentInputSaved === '' && operator === '-') {
+        this.currentInputSaved = '-';
+        return;
     }
-
-    appendNumber(number) { //appends the number to the current input
-        if (number === '.' && this.currentInput.includes('.')) return; //checks if the current input already contains a decimal point and returns if it does
-        this.currentInput = this.currentInput.toString() + number.toString(); //converts the number to a string and appends it to the current input
+    if (this.currentInputSaved === '') return;
+    if (this.resultOutputSaved !== '') {
+        this.currentInputSaved = this.resultOutputSaved;
+        this.resultOutputSaved = '';
     }
-
-    updateDisplay() { //updates the display
-        currentInput.innerText = this.currentInput; //sets the current input to the current input property
-    }
+    this.currentInputSaved += operator;
 }
 
-const calculator = new Calculator(currentInput, resultOutput); //creates a new calculator object
 
-numberButtons.forEach(button => { //adds an event listener to each number button
+    appendNumber(number) {
+        if (number === '.' && this.currentInputSaved.includes('.')) return; //checks if the current input already contains a decimal point and returns if it does
+        this.currentInputSaved = this.currentInputSaved.toString() + number.toString(); //converts the number to a string and appends it to the current input
+    }
+
+    toPostfix(expression) {
+        const precedence = {
+            '^': 4,
+            '*': 3,
+            '÷': 3,
+            '+': 2,
+            '-': 2,
+        };
+
+        const output = [];
+        const operators = [];
+
+        let numberBuffer = [];
+        let isNegative = false;
+
+        for (let token of expression) {
+            if (token === '-' && (numberBuffer.length === 0 || '*/+-'.includes(expression[expression.indexOf(token) - 1]))) {
+                isNegative = true;
+                continue;
+            }
+
+            if ('0123456789.'.includes(token)) {
+                numberBuffer.push(token);
+            } else {
+                if (numberBuffer.length > 0) {
+                    const number = isNegative ? '-' + numberBuffer.join('') : numberBuffer.join('');
+                    output.push(number);
+                    numberBuffer = [];
+                    isNegative = false;
+                }
+
+                while (operators.length > 0 && precedence[token] <= precedence[operators[operators.length - 1]]) {
+                    output.push(operators.pop());
+                }
+                operators.push(token);
+            }
+        }
+
+        if (numberBuffer.length > 0) {
+            const number = isNegative ? '-' + numberBuffer.join('') : numberBuffer.join('');
+            output.push(number);
+        }
+
+        while (operators.length > 0) {
+            output.push(operators.pop());
+        }
+
+        return output.join(' ');
+    }
+
+
+   evaluatePostfix(postfix) {
+    const stack = [];
+
+    postfix.split(' ').forEach((token) => {
+        if (!isNaN(token)) {
+            stack.push(parseFloat(token));
+        } else {
+            const b = stack.pop();
+            const a = stack.pop();
+            switch (token) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '÷':
+                    stack.push(a / b);
+                    break;
+                case '^':
+                    stack.push(Math.pow(a, b));
+                    break;
+            }
+        }
+    });
+
+    return stack.pop();
+    }
+
+
+    updateDisplay() {
+        currentInput.innerText = this.currentInputSaved;
+        resultOutput.innerText = this.resultOutputSaved;
+    }
+
+}
+
+const calculator = new Calculator(currentInput, resultOutput);
+
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText); //calls the numbers method
-        calculator.updateDisplay(); //calls the update display method
-        console.log(calculator); //logs the calculator object to the console
-    })
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+        console.log(calculator);
+    });
 });
 
-clearButton.addEventListener('click', () => { //adds an event listener to the clear button
-    calculator.clear(); //calls the clear method
-    calculator.updateDisplay(); //calls the update display method
+clearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+operatorButtons.forEach(operator => {
+    operator.addEventListener('click', () => {
+        calculator.operators(operator.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    calculator.equal();
+    calculator.updateDisplay();
 });
 
